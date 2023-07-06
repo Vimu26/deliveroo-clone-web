50031c905ff2c3c54847d94a22158d6c70948263
Akalanaka Rathnayake <128693618+Vimu26@users.noreply.github.com>
2023-07-06 17:21:14 +0530

    Revert "feat: :zap: added a button"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'deliveroo-clone-web';
-
-  onClick(){
-    alert("You Clicked the Button");
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import {MatButtonModule} from '@angular/material/button';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -13,7 +12,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatButtonModule,
     BrowserAnimationsModule
   ],
   providers: [],
