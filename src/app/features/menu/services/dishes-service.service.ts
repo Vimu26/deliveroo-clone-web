dd61ba3eada99904168d9876ed1dd56f034b973f
akalanka <akalanka@echonlabs.com>
2023-12-11 19:09:07 +0530

    menu items changes done

--- src/app/features/menu/services/dishes-service.service.ts
+++ src/app/features/menu/services/dishes-service.service.ts
@@ -8,9 +8,16 @@ import { HttpClient } from '@angular/common/http';
 })
 export class DishesServiceService {
   readonly apiURL = 'http://localhost:8080/dishes/';
+  readonly apiURL2 = 'http://localhost:8080/dishes/dish-categories/';
   constructor(private http: HttpClient) {}
 
   getAllDishes(): Observable<IDish[]> {
     return this.http.get<IDish[]>(this.apiURL + '');
+  }
+
+  // get dish by restaurant id and dish category id
+  getAllCategoryDishes(id: { restId: any; catId: any; }): Observable<IDish[]> {
+    const url = `${this.apiURL2}${id.restId}/${id.catId}`
+    return this.http.get<IDish[]>(url);
   }
 }
