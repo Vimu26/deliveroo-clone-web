5d9bc4a00bcbce9521d9945d1a460971b82d33e1
vimu26 <akalankavimukthi2@gmail.com>
2023-12-17 08:29:01 -0800

    dish changes done

--- src/app/features/menu/services/dishes-service.service.ts
+++ src/app/features/menu/services/dishes-service.service.ts
@@ -1,23 +1,18 @@
 import { Injectable } from '@angular/core'
 import { Observable } from 'rxjs'
-import { IDish } from '../../../interfaces'
-import { HttpClient } from '@angular/common/http'
+import { CommonResponse, IDish } from '../../../interfaces'
+import { HttpClient, HttpParams } from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root',
 })
 export class DishesServiceService {
-  readonly apiURL = 'http://localhost:8080/dishes/'
-  readonly apiURL2 = 'http://localhost:8080/dishes/dish-categories/'
   constructor(private http: HttpClient) {}
 
-  getAllDishes(): Observable<IDish[]> {
-    return this.http.get<IDish[]>(this.apiURL + '')
-  }
-
-  // get dish by restaurant id and dish category id
-  getAllCategoryDishes(id: { restId: any; catId: any }): Observable<IDish[]> {
-    const url = `${this.apiURL2}${id.restId}/${id.catId}`
-    return this.http.get<IDish[]>(url)
+  getAllDishes(params: HttpParams): Observable<CommonResponse<IDish[]>> {
+    const url = 'http://localhost:8080/dishes/'
+    return this.http.get<CommonResponse<IDish[]>>(url + '', {
+      params,
+    })
   }
 }
