561b6f5f1f9fe90dede4b1f42a7fd59f52126459
vimu26 <akalankavimukthi2@gmail.com>
2023-12-19 09:17:04 +0530

    restructuring

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
-import { MainComponent } from './components/main/main.component'
-import { NotFoundComponent } from './components/not-found/not-found.component'
+import { MainComponent } from './components/pages/landing-component/main/main.component'
+import { NotFoundComponent } from './components/pages/not-found/not-found.component'
 
 const routes: Routes = [
   { path: '', component: MainComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,14 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { HeaderComponent } from './components/header/header.component'
+import { HeaderComponent } from './components/pages/header/header.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { MainComponent } from './components/main/main.component'
+import { MainComponent } from './components/pages/landing-component/main/main.component'
 import { MaterialModule } from './material.module'
-import { FooterComponent } from '../../src/app/components/footer/footer.component'
-import { MainContentComponent } from './components/main-content/main-content.component'
+import { FooterComponent } from './components/pages/footer/footer.component'
+import { MainContentComponent } from './components/pages/landing-component/main-content/main-content.component'
 import { HttpClientModule } from '@angular/common/http'
-import { NotFoundComponent } from './components/not-found/not-found.component'
+import { NotFoundComponent } from './components/pages/not-found/not-found.component'
 
 @NgModule({
   declarations: [
