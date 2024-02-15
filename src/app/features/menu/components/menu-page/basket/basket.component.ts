e674aa2fee16ad048a0f392a045e4db66e9a5b0d
vimu26 <akalankavimukthi2@gmail.com>
2024-02-15 13:17:20 +0530

    getting data to basket

--- src/app/features/menu/components/menu-page/basket/basket.component.ts
+++ src/app/features/menu/components/menu-page/basket/basket.component.ts
@@ -1,14 +1,31 @@
-import { Component } from '@angular/core'
+import { Component, OnDestroy, OnInit } from '@angular/core'
+import { BasketService } from './services/basket.service'
+import { IAddedDishData } from 'src/app/interfaces'
+import { Subscription } from 'rxjs'
 
 @Component({
   selector: 'app-basket',
   templateUrl: './basket.component.html',
   styleUrls: ['./basket.component.scss'],
 })
-export class BasketComponent {
-  collectionElements: [] = []
+export class BasketComponent implements OnInit, OnDestroy {
+  addedToCart: IAddedDishData[] = []
+  subscription: Subscription = new Subscription()
+
+  constructor(private basketService: BasketService) {}
+
+  ngOnInit() {
+    this.subscription = this.basketService.addedToCart$.subscribe((data) => {
+      this.addedToCart = data
+      console.log(this.addedToCart)
+    })
+  }
 
   onClickCheckout() {
     //
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe()
   }
 }
--- src/app/features/menu/services/dishes-service.service.ts
+++ src/app/features/menu/services/dishes-service.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@angular/core'
-import { Observable } from 'rxjs'
-import { CommonResponse, IDish } from '../../../interfaces'
+import { BehaviorSubject, Observable, Subject } from 'rxjs'
+import {
+  CommonResponse,
+  DishAddOns,
+  IAddedDishData,
+  IDish,
+} from '../../../interfaces'
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http'
 
 @Injectable({
