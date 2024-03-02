f83b855577978b3f35bf4850de662a5a5361d621
vimu26 <akalankavimukthi2@gmail.com>
2024-03-03 00:42:10 +0530

    payment details part done

--- src/app/features/cart/components/cart/cart.component.ts
+++ src/app/features/cart/components/cart/cart.component.ts
@@ -3,6 +3,9 @@ import { Router } from '@angular/router'
 import { Subscription } from 'rxjs'
 import { BasketService } from 'src/app/features/menu/components/menu-page/basket/services/basket.service'
 import { AuthService } from 'src/app/features/auth/services/auth.service'
+import { userDetails } from 'src/app/interfaces'
+import { MatCheckboxChange } from '@angular/material/checkbox'
+import { FormControl, FormGroup } from '@angular/forms'
 
 export enum PAYMENT_METHOD {
   CASH = 'CASH',
@@ -20,12 +23,29 @@ export class CartComponent implements OnInit, OnDestroy {
   orderTotal: number = 0
   PAYMENT_METHOD = PAYMENT_METHOD
   selectedOption: string = PAYMENT_METHOD.CASH
+  userDetails : userDetails | undefined
+  selected: number = 0;
+
+  userForm = new FormGroup({
+    name: new FormControl(''),
+    address: new FormControl(''),
+    contactNumber: new FormControl(''),
+    totalAmount: new FormControl('')
+  });
 
   constructor(
     private basketService: BasketService,
     private router: Router,
     private authService: AuthService,
   ) {}
+
+  toggleSelection(event: MatCheckboxChange, value: number) {
+    if (event.checked && this.selected === value) {
+      this.selected = -1;
+    } else {
+      this.selected = value;
+    }
+  }
 
   ngOnInit(): void {
     this.subscription = this.basketService
@@ -49,7 +69,13 @@ export class CartComponent implements OnInit, OnDestroy {
     const token = localStorage.getItem('token') ?? ''
     this.authService.getUserByToken(token).subscribe({
       next: (res) => {
-        console.log(res)
+        this.userDetails = res.data
+        this.userForm.patchValue({
+          name: this.userDetails.full_name,
+          address: this.userDetails.address,
+          contactNumber: this.userDetails.contact_number,
+          totalAmount : `£${this.orderTotal}`,
+        });
       },
       error(err) {
         console.log(err.message)
