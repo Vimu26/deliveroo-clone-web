2de7bb34f8eaa12663f7b720e0f78a69f31dd274
vimu26 <akalankavimukthi2@gmail.com>
2024-03-18 11:20:58 +0530

    login added in restaurants

--- src/app/features/restaurant/components/restaurant/restaurant.component.ts
+++ src/app/features/restaurant/components/restaurant/restaurant.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { FormGroup, FormControl, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-restaurant',
   templateUrl: './restaurant.component.html',
-  styleUrls: ['./restaurant.component.scss']
+  styleUrls: ['./restaurant.component.scss'],
 })
-export class RestaurantComponent {
+export class RestaurantComponent implements OnInit {
+  hidePassword = true
+  form = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+  })
 
+  constructor() {}
+
+  ngOnInit(): void {
+    //
+  }
+  onClickAddRestaurant() {
+    //
+  }
+
+  togglePasswordVisibility() {
+    this.hidePassword = !this.hidePassword
+  }
+
+  onClickRestaurantLogin(){
+    //
+  }
 }
