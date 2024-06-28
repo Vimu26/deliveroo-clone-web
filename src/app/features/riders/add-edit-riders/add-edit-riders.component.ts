fa7a7eaecd3cff94a48dcf7d671071e11e67dbc5
vimu26 <akalankavimukthi2@gmail.com>
2024-06-28 07:01:34 +0530

    changes

--- src/app/features/riders/add-edit-riders/add-edit-riders.component.ts
+++ src/app/features/riders/add-edit-riders/add-edit-riders.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core'
 @Component({
   selector: 'app-add-edit-riders',
   templateUrl: './add-edit-riders.component.html',
-  styleUrls: ['./add-edit-riders.component.css'],
+  styleUrls: ['./add-edit-riders.component.scss'],
 })
 export class AddEditRidersComponent {}
