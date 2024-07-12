d94f4309edc1a9d9c1502712067e4dfef956a529
vimu26 <akalankavimukthi2@gmail.com>
2024-07-12 18:04:12 +0530

    route change

--- src/app/features/menu/menu-routing.module.ts
+++ src/app/features/menu/menu-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 import { MenuComponent } from './components/menu-page/menu.component'
 
-const menuRoutes: Routes = [{ path: '', component: MenuComponent }]
+const menuRoutes: Routes = [{ path: ':id', component: MenuComponent }]
 
 @NgModule({
   imports: [RouterModule.forChild(menuRoutes)],
